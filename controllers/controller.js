d5a72f566612e02e52cb3e5bd454b1e51e9ff97b
Pat <patrycjatr2001@gmail.com>
2024-03-21 09:56:30 +0100

    Check object availability

--- controllers/controller.js
+++ controllers/controller.js
@@ -19,8 +19,8 @@ const db = new sqlite3.Database('rezerwator.db', err => {
         imie          TEXT,
         nazwisko      TEXT,
         przedmiot_id  INTEGER REFERENCES przedmioty (przedmiot_id),
-        start         TEXT,
-        stop          TEXT
+        start         DATE,
+        stop          DATE
       )
     `);
 
@@ -28,62 +28,64 @@ const db = new sqlite3.Database('rezerwator.db', err => {
             if (err) {
                 console.error('Błąd sprawdzania liczby rekordów:', err);
             } else if (row.count === 0) {
+                db.run('INSERT INTO rezerwacje (imie, nazwisko, przedmiot_id, start, stop) VALUES (?, ?, ?, ?, ?)',
+                    ['Jan', 'Kowalski', 1517, '2024-03-01', '2024-03-02']);
+                db.run('INSERT INTO rezerwacje (imie, nazwisko, przedmiot_id, start, stop) VALUES (?, ?, ?, ?, ?)',
+                    ['Jan', 'Kowalski', 1517, '2024-01-01', '2024-01-31']);
+
+
                 db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
-                    ['Rzutnik', 'Rzutnik do prezentacji', 'A-0-07']);
+                    ['Stół do pingponga', 'Stół dla studentów i nie tylko', 'Korytarz sektor G-0']);
+                db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
+                    ['Stół do pingponga', 'Stół dla studentów i nie tylko', 'Korytarz sektor F-0']);
+
                 db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
                     ['Stół do pingponga', 'Stół dla studentów i nie tylko', 'Korytarz sektor G-1']);
+                db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
+                    ['Stół do pingponga', 'Stół dla studentów i nie tylko', 'Korytarz sektor F-1']);
+
+                db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
+                    ['Stół do pingponga', 'Stół dla studentów i nie tylko', 'Korytarz sektor G-2']);
+                db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
+                    ['Stół do pingponga', 'Stół dla studentów i nie tylko', 'Korytarz sektor F-2']);
+
+                db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
+                    ['Rzutnik', 'Rzutnik do prezentacji', 'A-0-05']);
+                db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
+                    ['Rzutnik', 'Rzutnik do prezentacji', 'A-0-10']);
+                db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
+                    ['Rzutnik', 'Rzutnik do prezentacji', 'A-0-06']);
+                db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
+                    ['Rzutnik', 'Rzutnik do prezentacji', 'A-0-07']);
+
+                db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
+                    ['Rzutnik', 'Rzutnik do prezentacji', 'A-1-05']);
+                db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
+                    ['Rzutnik', 'Rzutnik do prezentacji', 'A-1-10']);
+                db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
+                    ['Rzutnik', 'Rzutnik do prezentacji', 'A-1-06']);
+                db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
+                    ['Rzutnik', 'Rzutnik do prezentacji', 'A-1-07']);
+
+                db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
+                    ['Rzutnik', 'Rzutnik do prezentacji', 'A-2-05']);
+                db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
+                    ['Rzutnik', 'Rzutnik do prezentacji', 'A-2-10']);
+                db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
+                    ['Rzutnik', 'Rzutnik do prezentacji', 'A-2-06']);
+                db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
+                    ['Rzutnik', 'Rzutnik do prezentacji', 'A-2-07']);
             } else {
-                db.run('DELETE FROM przedmioty', (err) => {
+                db.run('DELETE FROM rezerwacje', (err) => {
                     if (err) {
                         console.error('Błąd usuwania danych:', err);
                     } else {
-                        console.log('Usunięto istniejące dane z tabeli przedmioty');
-
-                        // Dodanie nowych danych
-                        db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
-                            ['Stół do pingponga', 'Stół dla studentów i nie tylko', 'Korytarz sektor G-0']);
-                        db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
-                            ['Stół do pingponga', 'Stół dla studentów i nie tylko', 'Korytarz sektor F-0']);
-
-                        db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
-                            ['Stół do pingponga', 'Stół dla studentów i nie tylko', 'Korytarz sektor G-1']);
-                        db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
-                            ['Stół do pingponga', 'Stół dla studentów i nie tylko', 'Korytarz sektor F-1']);
-
-                        db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
-                            ['Stół do pingponga', 'Stół dla studentów i nie tylko', 'Korytarz sektor G-2']);
-                        db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
-                            ['Stół do pingponga', 'Stół dla studentów i nie tylko', 'Korytarz sektor F-2']);
-
-                        db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
-                            ['Rzutnik', 'Rzutnik do prezentacji', 'A-0-05']);
-                        db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
-                            ['Rzutnik', 'Rzutnik do prezentacji', 'A-0-10']);
-                        db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
-                            ['Rzutnik', 'Rzutnik do prezentacji', 'A-0-06']);
-                        db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
-                            ['Rzutnik', 'Rzutnik do prezentacji', 'A-0-07']);
-
-                        db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
-                            ['Rzutnik', 'Rzutnik do prezentacji', 'A-1-05']);
-                        db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
-                            ['Rzutnik', 'Rzutnik do prezentacji', 'A-1-10']);
-                        db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
-                            ['Rzutnik', 'Rzutnik do prezentacji', 'A-1-06']);
-                        db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
-                            ['Rzutnik', 'Rzutnik do prezentacji', 'A-1-07']);
-
-                        db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
-                            ['Rzutnik', 'Rzutnik do prezentacji', 'A-2-05']);
-                        db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
-                            ['Rzutnik', 'Rzutnik do prezentacji', 'A-2-10']);
-                        db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
-                            ['Rzutnik', 'Rzutnik do prezentacji', 'A-2-06']);
-                        db.run('INSERT INTO przedmioty (nazwa, opis, miejsce) VALUES (?, ?, ?)',
-                            ['Rzutnik', 'Rzutnik do prezentacji', 'A-2-07']);
-
-                    }
-                });
+                        console.log('Usunięto istniejące dane z tabeli rezerwacje');
+                    }});
+                db.run('INSERT INTO rezerwacje (imie, nazwisko, przedmiot_id, start, stop) VALUES (?, ?, ?, ?, ?)',
+                    ['Jan', 'Kowalski', 1517, '2024-03-01', '2024-03-02']);
+                db.run('INSERT INTO rezerwacje (imie, nazwisko, przedmiot_id, start, stop) VALUES (?, ?, ?, ?, ?)',
+                    ['Jan', 'Kowalski', 1517, '2024-01-01', '2024-01-31']);
             }
         });
     }
@@ -108,8 +110,28 @@ const getPrzedmiot = (przedmiotId, callback) => {
         if (!row) {
             return callback(new Error('Przedmiot nie został znaleziony'), null);
         }
-        callback(null, row);
+        return callback(null, row);
     });
 };
 
-module.exports = { db, getAllItems, getPrzedmiot };
+const getRezerwacje = (przedmiotId, callback) => {
+    const sql = 'SELECT * FROM rezerwacje WHERE przedmiot_id = ?';
+    db.all(sql, [przedmiotId], (err, rows) => {
+        if (err) {
+            return callback(err, null);
+        }
+        callback(null, rows);
+    });
+};
+
+const checkRezerwacja = (przedmiotId, date, callback) => {
+    const sql = 'SELECT * FROM rezerwacje WHERE przedmiot_id = ? AND DATE(start) <= DATE(?) AND DATE(stop) >= DATE(?)';
+    db.all(sql, [przedmiotId, date, date], (err, rows) => {
+        if (err) {
+            return callback(err, null);
+        }
+        callback(null, rows.length > 0);
+    });
+};
+
+module.exports = { db, getAllItems, getPrzedmiot, getRezerwacje, checkRezerwacja };
--- routes/przedmioty.js
+++ routes/przedmioty.js
@@ -2,6 +2,36 @@ var express = require('express');
 const controller = require("../controllers/controller");
 var router = express.Router();
 
+router.get('/:id/month/:miesiac', (req, res) => {
+  const { id, miesiac } = req.params;
+  const przedmiotId = parseInt(id);
+  const month = parseInt(miesiac);
+
+  const sql = `SELECT * FROM rezerwacje WHERE przedmiot_id = ? AND strftime('%m', start) = ?`;
+  controller.db.all(sql, [przedmiotId, month.toString().padStart(2, '0')], (err, rows) => {
+    if (err) {
+      console.error('Błąd pobierania danych:', err.message);
+      return res.status(500).json({ error: 'Wystąpił błąd podczas pobierania danych' });
+    }
+    const reservationsForMonth = {};
+    rows.forEach(row => {
+      reservationsForMonth[row.start] = true;
+    });
+    res.json(reservationsForMonth);
+  });
+});
+
+router.get('/:przedmiotId/:selectedDate', function(req, res, next) {
+  const przedmiotId = req.params.przedmiotId;
+  const selectedDate = req.params.selectedDate;
+  controller.checkRezerwacja(przedmiotId, selectedDate, (err, isReserved) => {
+    if (err) {
+      console.error('Błąd sprawdzania rezerwacji:', err);
+      return res.status(500).json({ error: 'Błąd sprawdzania rezerwacji' });
+    }
+    res.json({ isReserved });
+  });
+});
 router.get('/:przedmiot_id', function(req, res, next) {
   const przedmiotId = req.params.przedmiot_id;
   controller.getPrzedmiot(przedmiotId, (err, przedmiot) => {
@@ -9,7 +39,7 @@ router.get('/:przedmiot_id', function(req, res, next) {
       console.error('Błąd pobierania opisu przedmiotu:', err);
       return next(err);
     }
-    res.render('przedmiot', { title: 'Rezerwator', nazwa: przedmiot.nazwa, opis: przedmiot.opis, miejsce: przedmiot.miejsce });
+    res.render('przedmiot', { title: 'Rezerwator', pi: req.params.przedmiot_id, nazwa: przedmiot.nazwa, opis: przedmiot.opis, miejsce: przedmiot.miejsce });
   });
 });
 
